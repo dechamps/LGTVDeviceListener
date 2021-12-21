95bc7f981a283c0c73faf45e697bbbe217a4bd9a
Etienne Dechamps <etienne@edechamps.fr>
2021-12-21 20:26:10 +0000

    Add command line parsing

--- LGTVDeviceListener.cpp
+++ LGTVDeviceListener.cpp
@@ -1,1 +1,16 @@
-int main() {}
+#include <cxxopts.hpp>
+
+int main(int argc, char** argv) {
+	::cxxopts::Options options("LGTVDeviceListener", "LGTV Device Listener");
+	try {
+		options.parse(argc, argv);
+	}
+	catch (const std::exception& exception) {
+		std::cerr << "USAGE ERROR: " << exception.what() << std::endl;
+		std::cerr << std::endl;
+		std::cerr << options.help() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
