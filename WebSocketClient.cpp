3c02bd021cf816b7ef786b89f5a7440c05b986db
Etienne Dechamps <etienne@edechamps.fr>
2021-12-23 17:31:43 +0000

    Throw an exception on WebSocket errors

--- WebSocketClient.cpp
+++ WebSocketClient.cpp
@@ -32,7 +32,13 @@ namespace LGTVDeviceListener {
 			std::cerr << "OnMessageCallback(" << std::to_string(static_cast<int>(webSocketMessage->type)) << ")" << std::endl;
 
 			if (webSocketMessage->type == ix::WebSocketMessageType::Error) {
-				std::cerr << "WebSocket error: " << webSocketMessage->errorInfo.reason << std::endl;
+				const auto& errorInfo = webSocketMessage->errorInfo;
+				throw std::runtime_error(std::string("WebSocket client error: ") + errorInfo.reason +
+					" (retries: " + std::to_string(errorInfo.retries) +
+					", wait time: " + std::to_string(errorInfo.wait_time) +
+					" ms, HTTP status: " + std::to_string(errorInfo.http_status) +
+					(errorInfo.decompressionError ? ", decompression error" : "") +
+					")");
 			}
 		});
 
