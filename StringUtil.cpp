27b31e397759781848e1739ebd7d0a21ceca3539
Etienne Dechamps <etienne@edechamps.fr>
2022-04-10 11:22:37 +0100

    Make ToWideString() less brittle

--- StringUtil.cpp
+++ StringUtil.cpp
@@ -7,11 +7,11 @@ namespace LGTVDeviceListener {
 	std::wstring ToWideString(std::string_view input, UINT codePage) {
 		if (input.size() == 0) return {};
 
-		const auto size = ::MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, input.data(), int(input.size()), NULL, 0);
+		const auto size = ::MultiByteToWideChar(codePage, 0, input.data(), int(input.size()), NULL, 0);
 		if (size <= 0) throw std::system_error(std::error_code(::GetLastError(), std::system_category()), "Unable to get size for conversion to wide string");
 
 		std::wstring result(size, 0);
-		if (::MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, input.data(), int(input.size()), result.data(), int(result.size())) != int(result.size()))
+		if (::MultiByteToWideChar(codePage, 0, input.data(), int(input.size()), result.data(), int(result.size())) != int(result.size()))
 			throw std::system_error(std::error_code(::GetLastError(), std::system_category()), "Unable to convert to wide string");
 		return result;
 	}
